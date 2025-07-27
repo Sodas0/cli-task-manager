9d137c73ca38b2980322b4b84d2698efeaf05902
Sodas0 <dasso@oregonstate.edu>
2025-07-26 20:30:24 -0700

    feature: added controller to call correct command based on argument

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod cli;
 mod storage;
 
 
-use crate::cli::{parse_args, command_add};
+use crate::cli::{parse_args, command_add, command_view};
 use crate::storage::{create_json};
 
 use std::fs::File;
@@ -13,29 +13,44 @@ use std::fs::OpenOptions;
 fn main(){
     
     //move the database file creation and opening here so that the opened file can be passed anywhere
-    let fname: &'static str = "database.json";
+    let fpath: &'static str = "database.json";
     let json_file:File;
 
 
     // potential refactor , maybe just get rid of create_json() and put logic here.
 
-    if !Path::new(fname).exists() {
-        json_file = create_json(fname);
+    if !Path::new(fpath).exists() {
+        json_file = create_json(fpath);
     } else {
         json_file = OpenOptions::new()
             .read(true)
             .append(true)
-            .open(fname).unwrap();
+            .open(fpath).unwrap();
     }
 
     // parse arguments
     let parsed_args: Vec<String> = parse_args();
     
-    command_add(parsed_args, json_file);
+    
+    // controller that will direct to the correct command
 
-    //need controller here that will direct to correct command based on result of parse_args()
-    // use parsed_args to determine what command was requested.
+    let command = &parsed_args[0];
 
+    match command.as_str() {
+        "-add" => {
+            if parsed_args.len() < 2 {
+                eprintln!("Usage: -add <task_name>");
+            } else {
+                command_add(parsed_args, json_file);
+            }
+        }
+        "-view" => {
+            command_view(json_file).unwrap();
+        }
+        _ => {
+            eprintln!("Invalid command. Use -help for available options.");
+        }
+    }
     
     // command_add(parsed_args).unwrap(); // unwrap more elegantly 
     
