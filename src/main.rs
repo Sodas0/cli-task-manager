3b11159f0973732868e55731fda8791f0d6382e8
Sodas0 <dasso@oregonstate.edu>
2025-07-26 19:57:53 -0700

    refactor: move DB file creation to main for consistent command structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
-// use crate::task::Task;
-use crate::cli::{parse_args, command_add};
-
 mod task;
 mod cli;
+mod storage;
+
+
+use crate::cli::{parse_args, command_add};
+use crate::storage::{create_json};
+
+use std::fs::File;
+use std::path::Path;
+use std::fs::OpenOptions;
 
 fn main(){
-
-    // let name:String = args[1].clone();
-    // let id:u16 = 0;
-    //let task1 = Task{name:name, id:id};
     
+    //move the database file creation and opening here so that the opened file can be passed anywhere
+    let fname: &'static str = "database.json";
+    let json_file:File;
+
+
+    // potential refactor , maybe just get rid of create_json() and put logic here.
+
+    if !Path::new(fname).exists() {
+        json_file = create_json(fname);
+    } else {
+        json_file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(fname).unwrap();
+    }
+
+    // parse arguments
     let parsed_args: Vec<String> = parse_args();
-    command_add(parsed_args).unwrap(); // unwrap more elegantly 
+    
+    command_add(parsed_args, json_file);
+
+    //need controller here that will direct to correct command based on result of parse_args()
+    // use parsed_args to determine what command was requested.
+
+    
+    // command_add(parsed_args).unwrap(); // unwrap more elegantly 
     
 
 }
