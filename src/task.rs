55185238c9475e12153c31e12176cc7eef8ea2a9
Soham Das <soham.das.asm@gmail.com>
2025-08-11 21:27:24 -0700

    feature: serialize and deserialize functions for JSON db functional

--- src/task.rs
+++ src/task.rs
@@ -1,23 +1,9 @@
-use serde::ser::{Serialize, Serializer, SerializeStruct};
+use serde::{Serialize, Deserialize};
 
-
-
-// #[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Task{
     pub name: String, 
     pub id: u16,
     pub is_done: bool,    
-}
-impl Serialize for Task {
-    fn serialize<S>(&self, serializer:S) -> Result<S::Ok, S::Error>
-    where 
-        S: Serializer,
-        {
-            let mut state: <S as Serializer>::SerializeStruct = serializer.serialize_struct("Task", 3)?;
-            state.serialize_field("name", &self.name)?;
-            state.serialize_field("id", &self.id)?;
-            state.serialize_field("is_done", &self.is_done)?;
-            state.end()
-        }
 }
 
